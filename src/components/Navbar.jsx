6e7ee8b474b22729ea06793100383a3d78300614
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2024-03-06 16:57:38 +0100

    Upgrade mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
   };
 
   return (
-    <div className="w-full flex justify-around items-center">
+    <div className="w-full flex justify-evenly items-center">
       <a className="hover:text-purple-600 cursor-pointer m-4">
         <Link to={`/`}>
           <h1 className="text-3xl">FullStackSolutions</h1>
@@ -31,31 +31,28 @@ function Navbar() {
           <Link to={`/contactme`}> Contact me</Link>
         </li>
       </ul>
-      <div onClick={handleNav}>
+      <div onClick={handleNav} className="block md:hidden">
         {""}
-        {!nav ? (
-          <FiX
-            size={20}
-            className=" hover:text-purple-600 cursor-pointer md:hidden"
-          />
+        {nav ? (
+          <FiX size={20} className=" hover:text-purple-600 cursor-pointer" />
         ) : (
           <CgMenu
             size={20}
-            className=" hover:text-purple-600 cursor-pointer md:hidden"
+            className=" hover:text-purple-600 cursor-pointer "
           />
         )}
       </div>
 
       <div
         className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full ease-in-out duration-500 border-r border-r-gray-900 bg-gray-800 md:hidden"
-            : "fixed left-[-100%] md:hidden"
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full ease-in-out duration-500 border-r border-r-gray-900 bg-gray-800 "
+            : "ease-in-out duration-700 fixed left-[-100%] top-0 h-screen "
         }
       >
         <a className="hover:text-purple-600 cursor-pointer m-4">
           <Link to={`/`}>
-            <h1 className="text-3xl m-5">FullStackSolutions</h1>
+            <h1 className="text-3xl m-2">FullStackSolutions</h1>
           </Link>
         </a>
         <ul className=" uppercase p-4 ">
