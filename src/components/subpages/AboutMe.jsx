a2cfb75ec615ecec3e1c80acf7ecfe9e37f87b9a
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2024-03-11 17:53:31 +0100

    update about me component

--- src/components/subpages/AboutMe.jsx
+++ src/components/subpages/AboutMe.jsx
@@ -1,10 +1,33 @@
+import moto2 from "../../assets/moto2.jpg";
 import Navbar from "../Navbar";
 
 function AboutMe() {
   return (
-    <div className="w-screen h-screen text-gray-50 bg-custom-dark ">
+    <div className="w-full h-full text-gray-50 bg-custom-dark ">
       <Navbar />
-      About me page in progress...
+      <div className="w-full flex flex-col items-center">
+        <img
+          src={moto2}
+          alt="image"
+          className="w-[80%] max-w-md  md:h-4/5 rounded-3xl p-4 mt-6"
+        />
+        <div className="w-[80%] p-4">
+          <h2 className="text-xl">Hello, my name is Mateusz.</h2>
+          <p>
+            I've dedicated countless hours to coding over the past year, and my
+            GitHub profile is a testament to my commitment and progress. Through
+            consistent daily coding practice, I have consistently excelled in my
+            skills and expanded my knowledge of various programming languages
+            and frameworks. Through my IT internship at the Erasmus+ National
+            Agency, I gained valuable experience in the field of information
+            technology and SEO. Working as a website administrator and order
+            processor at Mbiotica allowed me to develop my Prestashop skills.
+            These lessons learned set me on a path of development as a
+            programmer, which resulted in the completion of a JavaScript
+            Developer bootcamp.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
